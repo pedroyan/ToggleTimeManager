4bfd04955238af9f49a6a9ae500e2b7645fe6059
Pedro Yan Ornelas <pedroyane@gmail.com>
2018-12-09 19:36:45 -0300

    Initial attempt at MVVM on WPF

---
+++ src/TogglTimeManager/ButtonCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TogglTimeManager
+{
+    public class ButtonCommand : ICommand
+    {
+        private readonly MainWindowViewModel _mainViewModel;
+
+        public ButtonCommand(MainWindowViewModel mainViewModel)
+        {
+            _mainViewModel = mainViewModel;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _mainViewModel.TextBox = "MVVM Rules!";
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
---
+++ src/TogglTimeManager/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TogglTimeManager.Annotations;
+
+namespace TogglTimeManager
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string _textBox;
+        public string TextBox
+        {
+            get => _textBox;
+            set
+            {
+                if (value == _textBox) return;
+                _textBox = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ICommand _buttonCommand;
+        public ICommand ButtonCommand => _buttonCommand ?? (_buttonCommand = new ButtonCommand(this));
+    }
+}
