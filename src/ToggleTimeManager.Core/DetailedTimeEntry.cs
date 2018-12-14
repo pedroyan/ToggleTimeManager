80f08524ae84a2dc09d8822990f2b668b21ce129
Pedro Yan Ornelas <pedroyane@gmail.com>
2018-12-14 14:41:09 -0300

    Added the detailed time entry sctructure

---
+++ src/ToggleTimeManager.Core/DetailedTimeEntry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToggleTimeManager.Core
+{
+    public class DetailedTimeEntry : TimeEntry
+    {
+        /// <summary>
+        /// The description of the entry
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// The start time of the entry
+        /// </summary>
+        public DateTime Start { get; set; }
+
+        /// <summary>
+        /// The end time of the entry
+        /// </summary>
+        public DateTime End { get; set; }
+    }
+}
