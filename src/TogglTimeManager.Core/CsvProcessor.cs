222acaeb89d40bf6aaeeddaf84fa9a90f88a335d
Pedro Yan Ornelas <pedroyane@gmail.com>
2018-12-18 18:32:02 -0200

    Improved error handling so application can display useful information to the user when errors occur

--- src/TogglTimeManager.Core/CsvProcessor.cs
+++ src/TogglTimeManager.Core/CsvProcessor.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using CsvHelper;
 using TogglTimeManager.Core.Models;
 using TogglTimeManager.Core.Parsing;
 
@@ -29,7 +30,8 @@ namespace TogglTimeManager.Core
                 throw new ArgumentException("The select file is not a CSV");
             }
 
-            List<TimeEntry> records = TogglCsvParser.ParseCsv(filePath);
+            var records = GetTimeEntries(filePath);
+
             var timeSheet = new TimeSheet
             {
                 TimeEntries = records.ToList(),
@@ -37,6 +39,30 @@ namespace TogglTimeManager.Core
             };
 
             return timeSheet;
+        }
+
+        private static List<TimeEntry> GetTimeEntries(string filePath)
+        {
+            try
+            {
+                return TogglCsvParser.ParseCsv(filePath);
+            }
+            catch (HeaderValidationException e)
+            {
+                throw new Exception("Invalid CSV Header", e);
+            }
+            catch (ValidationException e)
+            {
+                //TODO: Log original exception
+                throw new Exception("Invalid CSV", e);
+            }
+            catch (ReaderException e)
+            {
+                //User should know if the error was caused by a invalid field
+                if (e.InnerException is FormatException) throw e.InnerException;
+
+                throw e;
+            }
         }
 
         private static DateRange? ParseDateRangeFromFileName(string fileName)
--- src/TogglTimeManager.Core/Parsing/TimeConverter.cs
+++ src/TogglTimeManager.Core/Parsing/TimeConverter.cs
@@ -24,22 +24,22 @@ namespace TogglTimeManager.Core.Parsing
 
             if (splitTime.Length != 3)
             {
-                throw new FormatException($"The time format is invalid. Ensure the time on the csv follows the format (HH:mm:ss). Invalid input: {text}");
+                throw new FormatException($"The time format is invalid. Ensure the time on the csv follows the format (HH:mm:ss).\nValue: {text}, Line {row.Context.Row}");
             }
 
             if (!int.TryParse(splitTime[0], out var hour))
             {
-                throw new FormatException($"The hour is not a valid int. Invalid input: {text}");
+                throw new FormatException($"The hour is not a valid integer.\nValue: {text}, Line {row.Context.Row}");
             }
 
             if (!int.TryParse(splitTime[1], out var minutes))
             {
-                throw new FormatException($"The minute is not a valid int. Invalid input: {text}");
+                throw new FormatException($"The minute is not a valid integer.\nValue: {text}, Line {row.Context.Row}");
             }
 
             if (!int.TryParse(splitTime[2], out var seconds))
             {
-                throw new FormatException($"The second is not a valid int. Invalid input: {text}");
+                throw new FormatException($"The second is not a valid integer.\nValue: {text}, Line {row.Context.Row}");
             }
 
             var days = hour / 24;
